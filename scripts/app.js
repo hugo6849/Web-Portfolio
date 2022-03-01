f378e1c0ef26087e36e75072e6d63e14a052bdfe
Hugo Ruiz <76759774+hugo6849@users.noreply.github.com>
2022-03-01 10:29:17 -0500

    New Fixes
    Changes

--- scripts/app.js
+++ scripts/app.js
@@ -2,3 +2,8 @@ const text = document.querySelector(".about-1");
 const strText = text.textContent;
 
 console.log(strText);
+
+$(function(){
+$("#nav").load("navbar.html"); 
+$("#contact").load("contact.html"); 
+});
