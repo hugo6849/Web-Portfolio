c2a1d01095068f47e1721b0b611c69d7977bf696
Hugo Ruiz <76759774+hugo6849@users.noreply.github.com>
2022-03-28 19:51:21 -0400

    Personal Portfolio Updates
    Massive changes to personal portfolio

--- scripts/app.js
+++ scripts/app.js
@@ -2,8 +2,13 @@ const text = document.querySelector(".about-1");
 const strText = text.textContent;
 
 console.log(strText);
+let about_description = document.getElementById('about-description');
 
 $(function(){
 $("#nav").load("navbar.html"); 
 $("#contact").load("contact.html"); 
+$("#photography").load("photography.html")
 });
+
+/**Document Listener Events*/
+document.addEventListener('scroll', animate);
